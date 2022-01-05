a2ef95d16e4e7aa8c6f9feefd892befa856d93dd
iamidlek <iamidlek@naver.com>
2022-01-05 20:20:58 +0900

    feat: login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import { Login } from "./components";
+import Home from "./container/Home";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+    // if (!User) navigate("/login");
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
